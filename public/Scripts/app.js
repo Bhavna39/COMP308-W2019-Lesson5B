33caaad2f9da2a2c795bd56da0dc444d3bd771d7
Bhavna Pulliahgari <43325994+BhavnaP@users.noreply.github.com>
2019-01-26 17:12:11 -0500

    added bootstrap

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,12 @@
 /* Custom javascript */
+
+//IIFE - immediately invoked function expression
+(function()
+{
+    function Start()
+    {
+        console.log(`%c App Started......`, "font-size: 20px; color: blue; font-weight: bold");
+    }
+
+    window.addEventListener("load", Start);
+})();
