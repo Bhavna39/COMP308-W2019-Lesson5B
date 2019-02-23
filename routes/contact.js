257e92adc15077657396271cae023e1342968842
Bhavna Pulliahgari <43325994+BhavnaP@users.noreply.github.com>
2019-02-23 12:05:29 -0500

    added Create Functionality to the DB

--- routes/contact.js
+++ routes/contact.js
@@ -1,28 +1,52 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
 //schema - create a reference to the db schema
-let contact = require('../models/contact');
+let contactModel = require("../models/contact");
 
 /* GET Contact List page - Read operation */
 
-router.get('/', (req, res, next) => {
-    contact.find((err, contactList) => {
-            if(err){
-                return console.error(err);
-            }
-            else{
-               // console.log(contactList);
+router.get("/", (req, res, next) => {
+  contactModel.find((err, contactList) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      // console.log(contactList);
 
-                res.render('contacts/index', {
-                    title: 'List of Favourite Things',
-                    contactList: contactList
-                });
-                
-            }
-    });
+      res.render("contacts/index", {
+        title: "List of Favourite Things",
+        contactList: contactList
+      });
+    }
+  });
 });
 
+/* GET Route for th Add Contact page - READ Operation */
+router.get("/add", (req, res, next) => {
+  res.render("contacts/add", {
+    title: "Add New Contact"
+  });
+});
 
+/* POST Route for processing the Add page*/
+
+router.post("/add", (req, res, next) => {
+  //   console.log(req.body);
+
+  let newContact = contactModel({
+    Name: req.body.Name,
+    Description: req.body.Description
+  });
+
+  contactModel.create(newContact, (err, contactModel) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //refresh the contact list
+      res.redirect("/contact-list");
+    }
+  });
+});
 module.exports = router;
